7134bc125fd264f2f90fdfb52da7389db3b06d78
Redmar van den Berg <RedmarvandenBerg@lumc.nl>
2020-03-12 14:01:29 +0100

    Fix bug when a job was manually cancelled

--- slurm-cluster-status.py
+++ slurm-cluster-status.py
@@ -21,23 +21,25 @@ STATE_MAP = {
 }
 
 
-def fetch_statuses(batch_id):
-    """fetch all statuses for the batch id.
-    A batch id _may_ have multiple statuses"""
-    sacct_args = ["sacct", "-j",  batch_id, "-o", "State", "--parsable"]
+def fetch_status(batch_id):
+    """fetch the status for the batch id"""
+    sacct_args = ["sacct", "-j",  batch_id, "-o", "State", "--parsable2",
+                  "--noheader"]
     output = subprocess.check_output(sacct_args).decode("utf-8").strip()
-    return set(map(lambda x: x.strip("|"), output.split("\n")[1:]))
 
+    # The first output is the state of the overall job
+    # See
+    # https://stackoverflow.com/questions/52447602/slurm-sacct-shows-batch-and-extern-job-names
+    # for details
+    job_status = output.split("\n")[0]
 
-def final_state(statuses):
-    """Get the final state on a list of snakemake statuses"""
-    if "failed" in statuses:
-        return "failed"
-    if "running" in statuses:
-        return "running"
-    if "success" in statuses:
-        return "success"
-    raise NotImplementedError
+    # If the job was cancelled manually, it will say by who, e.g "CANCELLED by 12345"
+    # We only care that it was cancelled
+    if job_status.startswith("CANCELLED by"):
+        return "CANCELLED"
+
+    # Otherwise, return the status
+    return job_status
 
 
 if __name__ == "__main__":
@@ -45,7 +47,6 @@ if __name__ == "__main__":
     parser.add_argument("batch_id", type=str)
     args = parser.parse_args()
 
-    statuses = fetch_statuses(args.batch_id)
-    snakemake_statuses = set(map(lambda x: STATE_MAP[x.upper()], statuses))
+    status = fetch_status(args.batch_id)
 
-    print(final_state(snakemake_statuses))
+    print(STATE_MAP[status])
